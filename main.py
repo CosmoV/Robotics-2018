04dbed940ff862ed6c85ef50d6549f1202cba063
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2018-11-11 09:58:39 +1000

    [Pulsed Width Modulation] Add PWM handler

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-# import pyb
-# import time
-# import us016
-# import tcs34725
-# import vl53l0x
-# from machine import Pin, I2C
-#
+import pyb
+import time
+import us016
+import tcs34725
+import vl53l0x
+from machine import Pin, I2C
+
+PWMA = Pin('X1', Pin.OUT)
+AIN2 = Pin('X2', Pin.OUT)
+AIN1 = Pin('X3', Pin.OUT)
+STBY = Pin('X4', Pin.OUT)
+BIN1 = Pin('X5', Pin.OUT)
+BIN2 = Pin('X6', Pin.OUT)
+PWMB = Pin('X7', Pin.OUT)
+
+STBY.on()
+AIN1.on()
+AIN2.off()
+
 # i2c = I2C(scl=Pin('Y9', Pin.OUT), sda=Pin('Y10', Pin.OUT), freq=400000)
 # laser_sensor = vl53l0x.VL53L0X(i2c, 0x29)
 # laser_sensor.start_continuous(0)
@@ -16,7 +28,7 @@
 # rgb_sensor.gain(60)
 #
 # while True:
-    # print(tcs34725.html_rgb(sensor.read(True)))
-    # print(us_sensor.distance())
-    # print(laser_sensor.read_range_continuous_mm())
+#     print(tcs34725.html_rgb(sensor.read(True)))
+#     print(us_sensor.distance())
+#     print(laser_sensor.read_range_continuous_mm())
     
