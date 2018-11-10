e6c23bf65f5930c04ae69823ea1066c0bef96a6e
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2018-11-10 23:35:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # import time
 # import us016
 # import tcs34725
-# from machine import I2C, Pin
+# import vl53l0x
+# from machine import Pin, I2C
 #
-# i2c = I2C(scl=Pin('Y9', Pin.OUT), sda=Pin('Y10', Pin.OUT))
+# i2c = I2C(scl=Pin('Y9', Pin.OUT), sda=Pin('Y10', Pin.OUT), freq=400000)
+# laser_sensor = vl53l0x.VL53L0X(i2c, 0x29)
+# laser_sensor.start_continuous(0)
+#
 # rgb_sensor = tcs34725.TCS34725(i2c)
 # us_sensor = us016.Ultrasonic(pyb.Pin.board.X3, pyb.Pin.board.X4, 3, 3)
 #
@@ -12,6 +16,7 @@
 # rgb_sensor.gain(60)
 #
 # while True:
-#     print(tcs34725.html_rgb(sensor.read(True)))
-#     print(us_sensor.distance())
-#     pyb.delay(100)
+    # print(tcs34725.html_rgb(sensor.read(True)))
+    # print(us_sensor.distance())
+    # print(laser_sensor.read_range_continuous_mm())
+    
